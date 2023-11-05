7b4e9f5ad45a8b0c7fe2cc2a3a7c0747c9e82170
smahidv <smahioumaima10@gmail.com>
2023-11-05 11:57:04 +0100

    desable translate

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -6,10 +6,17 @@ import "./navigation.css";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
-  function toggleMenu(){
+
+  function toggleMenu() {
     setIsOpen(!isOpen);
     document.body.style.overflowY = isOpen ? "visible" : "hidden";
-    document.body.style.height = isOpen  ? "auto" : "100vh";
+    document.body.style.height = isOpen ? "auto" : "100vh";
+  }
+
+  function closeMenu() {
+    setIsOpen(false);
+    document.body.style.overflowY = "visible";
+    document.body.style.height = "auto";
   }
 
   useEffect(() => {
@@ -48,16 +55,16 @@ const Navigation = () => {
         <div className={`nav-options  absolute ${isOpen ? 'open' :''}`}>
           <ul className="nav-options-list">
             <li>
-              <Link to="#home">Home</Link>
+              <Link to="#home" onClick={closeMenu}>Home</Link>
             </li>
             <li>
-              <Link to="#about">About</Link>
+              <Link to="#about" onClick={closeMenu}>About</Link>
             </li>
             <li>
-              <Link to="#projects">Projects</Link>
+              <Link to="#projects" onClick={closeMenu}>Projects</Link>
             </li>
             <li className="shared-style-red-botton">
-              <Link to="#contact">Let's Talk</Link>
+              <Link to="#contact" onClick={closeMenu}>Let's Talk</Link>
             </li>
           </ul>
         </div>
